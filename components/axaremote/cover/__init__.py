1f534b3daadb1fdfc5fcb18d8f5d8be7fd9b8987
Rogier van Staveren <rogier@batoid.com>
2026-02-02 03:19:17 +0100

    Make detection of AXA Remote optional

--- components/axaremote/cover/__init__.py
+++ components/axaremote/cover/__init__.py
@@ -10,11 +10,13 @@ DEPENDENCIES = ['cover', 'uart']
 axaremote_ns = cg.esphome_ns.namespace('axaremote')
 AXARemoteCover = axaremote_ns.class_('AXARemoteCover', cg.Component, cover.Cover, uart.UARTDevice)
 
+CONF_DETECT = "detect"
 CONF_AUTO_CALIBRATE = "auto_calibrate"
 
 CONFIG_SCHEMA = cover.cover_schema(AXARemoteCover).extend(
     {
         cv.GenerateID(): cv.declare_id(AXARemoteCover),
+        cv.Optional(CONF_DETECT, default=False): bool,
         cv.Optional(CONF_POLLING_INTERVAL, default="0s"): cv.positive_time_period_milliseconds,
         cv.Optional(CONF_CLOSE_DURATION, default="50s"): cv.positive_time_period_milliseconds,
         cv.Optional(CONF_AUTO_CALIBRATE, default=False): bool,
@@ -28,6 +30,7 @@ async def to_code(config):
     await cover.register_cover(var, config)
     await uart.register_uart_device(var, config)
 
+    cg.add(var.set_detect(config[CONF_DETECT]))
     cg.add(var.set_polling_interval(config[CONF_POLLING_INTERVAL]))
     cg.add(var.set_close_duration(config[CONF_CLOSE_DURATION]))
     cg.add(var.set_auto_calibrate(config[CONF_AUTO_CALIBRATE]))
